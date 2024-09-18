96dbf6f2948a5a2f909c765790d2978a92f4f9ab
Anjan Upreti <anjanuprety787@gmail.com>
2024-09-18 18:14:10 +0545

    completion = 99.99%

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,33 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
-  },
-  plugins: [],
-
-  theme: {
     extend: {
       backgroundImage: {
-        'custom-gradient': 'linear-gradient(to bottom, #452e5e, #64527b, #847799, #a69eb8, #cbc7d6, #d0c9d7, #d5cbd7, #dacdd7, #c6a7af, #af8481, #8f6650, #644e23);',
+        'custom-gradient': 'linear-gradient(to left top, #2f598a, #2f547d, #304e70, #314963, #314357, #364759, #3b4b5c, #404f5e, #4c5f70, #577083, #638196, #6f93a9)',
+      },
+      keyframes: {
+        gradientAnimation: {
+          '0%': {
+            backgroundImage: 'linear-gradient(to left top, #2f598a, #2f547d, #304e70, #314963, #314357, #364759, #3b4b5c, #404f5e, #4c5f70, #577083, #638196, #6f93a9)',
+            backgroundSize: '300% 300%',
+            backgroundPosition: '0% 0%',
+          },
+          '50%': {
+            backgroundImage: 'linear-gradient(to left top, #6f93a9, #638196, #577083, #4c5f70, #404f5e, #3b4b5c, #364759, #314357, #314963, #304e70, #2f547d, #2f598a)',
+            backgroundSize: '300% 300%',
+            backgroundPosition: '100% 100%',
+          },
+          '100%': {
+            backgroundImage: 'linear-gradient(to left top, #2f598a, #2f547d, #304e70, #314963, #314357, #364759, #3b4b5c, #404f5e, #4c5f70, #577083, #638196, #6f93a9)',
+            backgroundSize: '300% 300%',
+            backgroundPosition: '0% 0%',
+          },
+        },
+      },
+      animation: {
+        gradient: 'gradientAnimation 12s ease-in-out infinite',
       },
     },
   },
   plugins: [],
 }
-
